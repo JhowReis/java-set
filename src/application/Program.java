281fe3589085e9f999eb78364f3c48dec1fbfad2
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-06-22 09:50:11 -0300

    Ending Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,48 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter file full path:  ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(" ");
+				String userName = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(userName, moment));
+				
+				line = br.readLine();
+				
+			}
+			System.out.println("Total users: " + set.size());
+			
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 
+		
+		sc.close();
 	}
 
 }
